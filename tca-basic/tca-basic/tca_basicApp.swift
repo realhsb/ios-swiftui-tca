377b322f8bff8052f6b8a4a4903bb183719589e9
realhsb <realhsb@gmail.com>
2024-08-25 02:47:24 +0900

    🎉

---
+++ tca-basic/tca-basic/tca_basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_basicApp.swift
+//  tca-basic
+//
+//  Created by Subeen on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct tca_basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

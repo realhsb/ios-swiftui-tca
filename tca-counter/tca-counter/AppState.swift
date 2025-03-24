578a7c4f3b06cc8e5098ee2b39ab619842d018f5
숩 <realhsb@gmail.com>
2025-03-24 21:05:52 +0900

    enum 전용 getter setter 생성

--- tca-counter/tca-counter/AppState.swift
+++ tca-counter/tca-counter/AppState.swift
@@ -48,6 +48,39 @@ enum AppAction {
     case counter(CounterAction)
     case primeModal(PrimeModalAction)
     case favoritePrimes(FavoritePrimesAction)
+    
+    var counter: CounterAction? {
+        get {
+          guard case let .counter(value) = self else { return nil }
+          return value
+        }
+        set {
+          guard case .counter = self, let newValue = newValue else { return }
+          self = .counter(newValue)
+        }
+      }
+
+      var primeModal: PrimeModalAction? {
+        get {
+          guard case let .primeModal(value) = self else { return nil }
+          return value
+        }
+        set {
+          guard case .primeModal = self, let newValue = newValue else { return }
+          self = .primeModal(newValue)
+        }
+      }
+
+      var favoritePrimes: FavoritePrimesAction? {
+        get {
+          guard case let .favoritePrimes(value) = self else { return nil }
+          return value
+        }
+        set {
+          guard case .favoritePrimes = self, let newValue = newValue else { return }
+          self = .favoritePrimes(newValue)
+        }
+      }
 }
 
 
@@ -177,7 +210,7 @@ struct EnumKeyPath<Root, Value> {
 }
 
 let _appReducer = combine(
-    pullback(counterReducer, value: \.count),
+//    pullback(counterReducer, value: \.count),
     primeModalReducer,
     pullback(favoritePrimesReducer, value: \.favoritePrimesState)
 )
